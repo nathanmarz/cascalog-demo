4ea3631fa96b451e041a0ef110c26e26d6dce6f2
Nathan Marz <nathan.marz@gmail.com>
2010-08-17 23:26:23 -0700

    bump cascalog version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [cascalog "1.1.0-SNAPSHOT"]
+                 [cascalog "1.2.0-SNAPSHOT"]
                  ]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]
-  :namespaces [cascalog-demo.demo])
+  :namespaces [cascalog-demo.demo cascalog-demo.demo2])
