a9a0d04c3a07b3a30ffe66a1d041776ccdf4dbb2
Olivier Grisel <olivier.grisel@ensta.org>
2010-09-05 15:25:18 +0200

    update project.clj to build against clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cascalog-demo "1.0.0-SNAPSHOT"
   :source-path "src/clj"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [cascalog "1.2.0-SNAPSHOT"]
                  ]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]
-  :namespaces [cascalog-demo.demo cascalog-demo.demo2])
+  :namespaces [cascalog-demo.demo])
