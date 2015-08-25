9ab635d6a01ef0bc8d910fdf1056184d330f3f93
Tatsuya BIZENN <bizenn@gmail.com>
2015-08-25 17:45:58 +0900

    Fix: chatch up: clojure 1.7.0, cascalog 2.1.1 and hadoop 2.5.2.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject cascalog-demo "1.0.0-SNAPSHOT"
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cascalog "1.10.0"]
-                 ]
-  :profiles { :dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]}}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cascalog "2.1.1"]
+                 [org.apache.hadoop/hadoop-client "2.5.2"]]
   :aot [cascalog-demo.demo]
   :main cascalog-demo.demo
 )
